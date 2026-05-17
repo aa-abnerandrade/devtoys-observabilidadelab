3617fd2e437ddee81a7622636deca3006a02a4c7
abnerandrade <aa.abnerandrade@outlook.com.br>
2026-05-17 20:02:35 -0300

    refactor(configs): rename application and test classes for consistency

--- src/main/java/com/devtoys/observabilidadelab/ObservabilidadeLabApplication.java
+++ src/main/java/com/devtoys/observabilidadelab/ObservabilidadeLabApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ObservabilidadelabApplication {
+public class ObservabilidadeLabApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ObservabilidadelabApplication.class, args);
+		SpringApplication.run(ObservabilidadeLabApplication.class, args);
 	}
 
 }
--- src/main/java/com/devtoys/observabilidadelab/exception/ExceptionHandler.java
+++
@@ -1,6 +0,0 @@
-package com.devtoys.observabilidadelab.exception;
-
-// Placeholder class. The real global handler is implemented in GlobalExceptionHandler.
-class ExceptionHandlerPlaceholder {
-
-}
